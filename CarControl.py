2c9897982114119b5d24c924bb62cfb5f298407e
Unknown <billy890116@gmail.com>
2019-07-17 22:41:18 +0800

    CarControl with class
    use class to program

--- CarControl.py
+++ CarControl.py
@@ -1,102 +1,128 @@
-# Simple demo of of the PCA9685 PWM servo/LED controller library.
-# This will move channel 0 from min to max position repeatedly.
-# Author: Tony DiCola
-# License: Public Domain
-from __future__ import division
 import time
 import Adafruit_PCA9685
-
-# Initialise the PCA9685 using the default address (0x40).
-pwm = Adafruit_PCA9685.PCA9685()
-
-pwm.set_pwm_freq(60)
-
-Forward_Speed = 500
-Backward_Speed = 270
-Status = 0
-
 import curses
- 
-# get the curses screen window
-screen = curses.initscr()
 
-curses.noecho()
-curses.cbreak()
-screen.keypad(True)
-curses.halfdelay(5)
+class CarControl() :
+    def __init__(self) :
+        self.pwm = Adafruit_PCA9685.PCA9685()
+        self.screen = curses.initscr()
 
-#reset
-pwm.set_pwm(1, 1, 400)
-pwm.set_pwm(3, 3, 400)
+        self.pwm.set_pwm_freq(60)
 
-'''
-Please use arrow key to control the car
-You can press w or s to change the speed when it wa s moving
-Press w will make uit faster
-'''
+        self.Forward_Speed = 500
+        self.Backward_Speed = 270
+        self.Status = 0
 
-try:
-    while True:
-        char = screen.getch()
-        if char == curses.KEY_RIGHT:
-            screen.addstr(0, 0, 'Enter : Right   ')
-            pwm.set_pwm(1, 1, 300)
-        elif char == curses.KEY_LEFT:
-            screen.addstr(0, 0, 'Enter : Left    ')
-            pwm.set_pwm(1, 1, 500)
-        elif char == ord(' '):
-            screen.addstr(0, 0, 'Enter : STOP    ')
-            pwm.set_pwm(3, 3, 400)
-            Status = 0
-        elif char == curses.KEY_UP:
-            screen.addstr(0, 0, 'Enter : Forward ')
-            pwm.set_pwm(3, 3, 400)
-            pwm.set_pwm(3, 3, Forward_Speed)
-            Status = 1
-        elif char == curses.KEY_DOWN: 
-            if Status == 0:
-                screen.addstr(0, 0, 'Enter : Backward')
-                pwm.set_pwm(3, 3, Backward_Speed)
-                Status = 2
-            elif Status == 1 :
-                screen.addstr(0, 0, 'Enter : Stop    ')
-                pwm.set_pwm(3, 3, 200)
-                time.sleep(0.1)
-                pwm.set_pwm(3, 3, 400)
-                Status = 0
-        elif char == -1:
-            screen.addstr(0, 0, 'Enter :         ')
-            pwm.set_pwm(1, 1, 400)
+        curses.noecho()
+        curses.cbreak()
+        self.screen.keypad(True)
+        curses.halfdelay(5)
 
+        self.pwm.set_pwm(1, 1, 400)
+        self.pwm.set_pwm(3, 3, 400)
 
-        elif char == ord('w') :
-            if Status == 1 and Forward_Speed < 650 :
-                Forward_Speed = Forward_Speed + 30
-                pwm.set_pwm(3, 3, Forward_Speed)
-                screen.addstr(0, 0, 'FASTER          ')
-            elif Status == 2 and Backward_Speed > 190 :
-                Backward_Speed = Backward_Speed - 30
-                pwm.set_pwm(3, 3, Backward_Speed)
-                screen.addstr(0, 0, 'FASTER          ') 
-        elif char == ord('s') :
-            if Status == 1 and Forward_Speed > 440 :
-                Forward_Speed = Forward_Speed - 30                                  
-                pwm.set_pwm(3, 3, Forward_Speed)
-                screen.addstr(0, 0, 'SLOWER          ')
-            elif Status == 2 and Backward_Speed < 350 :
-                Backward_Speed = Backward_Speed + 30
-                pwm.set_pwm(3, 3, Backward_Speed)
-                screen.addstr(0, 0, 'SLOWER          ')      
+    def reset(self) :
+        self.pwm.set_pwm(3, 3, 400)
 
-            
-except KeyboardInterrupt :
-    print('EXIT....   ')
-    pwm.set_pwm(1, 1, 400)
-    time.sleep(0.5)
-    pwm.set_pwm(3, 3, 400)
-    time.sleep(0.5)
-    curses.nocbreak(); screen.keypad(0); curses.echo()
-    curses.endwin()
+    def straight(self) :
+        self.screen.clear()
+        if self.Status == 0 :
+            self.screen.addstr(0, 0, 'Stop')
+        elif self.Status == 1 :
+            self.screen.addstr(0, 0, 'Forward')
+        elif self.Status == 2 :
+            self.screen.addstr(0, 0, 'Backward')
+        self.pwm.set_pwm(1, 1, 400)
+
+    def forward(self) :
+        self.screen.addstr(0, 0, 'Forward')
+        self.reset()
+        self.pwm.set_pwm(3, 3, self.Forward_Speed)
+        self.Status = 1
+
+    def backward(self) :
+        self.screen.addstr(0, 0, 'Backward')
+        self.pwm.set_pwm(3, 3, self.Backward_Speed)
+        self.Status = 2
+
+    def turn_right(self) :
+        self.screen.addstr(0, 0, 'Right')
+        self.pwm.set_pwm(1, 1, 300)
+
+    def turn_left(self) :
+        self.screen.addstr(0, 0, 'Left')
+        self.pwm.set_pwm(1, 1, 500)
+
+    def stop(self) :
+        self.screen.clear()
+        self.screen.addstr(0, 0, 'STOP')
+        self.pwm.set_pwm(3, 3, 400)
+        self.Status = 0
+
+    def speed_up(self, speed) :
+        if self.Status == 1 and self.Forward_Speed < 650 :
+            self.Forward_Speed += speed
+            self.pwm.set_pwm(3, 3, self.Forward_Speed)
+            self.screen.addstr(0, 0, 'FASTER')
+        elif self.Status == 2 and self.Backward_Speed > 190 :
+            self.Backward_Speed -= speed
+            self.pwm.set_pwm(3, 3, self.Backward_Speed)
+            self.screen.addstr(0, 0, 'FASTER') 
+
+    def speed_down(self, speed) :
+        if self.Status == 1 and self.Forward_Speed > 440 :
+            self.Forward_Speed -= speed
+            self.pwm.set_pwm(3, 3, self.Forward_Speed)
+            self.screen.addstr(0, 0, 'SLOWER')
+        elif self.Status == 2 and self.Backward_Speed < 350 :
+            self.Backward_Speed += speed
+            self.pwm.set_pwm(3, 3, self.Backward_Speed)
+            self.screen.addstr(0, 0, 'SLOWER')   
+
+    def exit(self) :
+        self.screen.clear()
+        self.screen.addstr(0, 0, 'EXIT...')
+        self.pwm.set_pwm(1, 1, 400)
+        time.sleep(0.5)
+        self.pwm.set_pwm(3, 3, 400)
+        time.sleep(0.5)
+        curses.nocbreak(); self.screen.keypad(0); curses.echo()
+        curses.endwin()
+
+def main() :
+    a = CarControl()
+    screen = curses.initscr()
+    screen.keypad(True)
+    curses.noecho()
+    curses.cbreak()
+    curses.halfdelay(5)
+
+    try :
+        while True :
+            char = screen.getch()
+            screen.clear()
+            if char == curses.KEY_RIGHT :
+                a.turn_right()
+            elif char == curses.KEY_LEFT :
+                a.turn_left()
+            elif char == curses.KEY_UP :
+                a.forward()
+            elif char == curses.KEY_DOWN :
+                a.backward()
+            elif char == ord(' ') :
+                a.stop()
+            elif char == ord('w') :
+                a.speed_up(30)
+            elif char == ord('s') :
+                a.speed_down(30)
+            elif char == -1 :
+                a.straight()
+                a.stop()
+    except KeyboardInterrupt :
+        a.exit()
+
+if __name__ == '__main__':
+    main()
 
 '''
 可設定的速度約為412~656, 522變523時燈號會從紅色變綠色
