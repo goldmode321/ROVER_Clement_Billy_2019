8c7be653d0e2c0a01f9c3e23567fb44c2546938b
goldmode321 <s148641@gmail.com>
2019-07-18 17:17:05 +0800

    switch directory

--- Clement/CarControl.py
+++ Clement/CarControl.py
@@ -7,9 +7,9 @@ import time
 import Adafruit_PCA9685
 
 # Initialise the PCA9685 using the default address (0x40).
-pwm = Adafruit_PCA9685.PCA9685()
 
-pwm.set_pwm_freq(60)
+
+
 
 Forward_Speed = 500
 Backward_Speed = 270
@@ -26,8 +26,7 @@ screen.keypad(True)
 curses.halfdelay(5)
 
 #reset
-pwm.set_pwm(1, 1, 400)
-pwm.set_pwm(3, 3, 400)
+
 
 '''
 Please use arrow key to control the car
@@ -38,63 +37,58 @@ Press w will make uit faster
 try:
     while True:
         char = screen.getch()
+        print(char)
         if char == curses.KEY_RIGHT:
             screen.addstr(0, 0, 'Enter : Right   ')
-            pwm.set_pwm(1, 1, 300)
+
         elif char == curses.KEY_LEFT:
             screen.addstr(0, 0, 'Enter : Left    ')
-            pwm.set_pwm(1, 1, 500)
+
         elif char == ord(' '):
             screen.addstr(0, 0, 'Enter : STOP    ')
-            pwm.set_pwm(3, 3, 400)
+
             Status = 0
         elif char == curses.KEY_UP:
             screen.addstr(0, 0, 'Enter : Forward ')
-            pwm.set_pwm(3, 3, 400)
-            pwm.set_pwm(3, 3, Forward_Speed)
+
+
             Status = 1
         elif char == curses.KEY_DOWN: 
             if Status == 0:
                 screen.addstr(0, 0, 'Enter : Backward')
-                pwm.set_pwm(3, 3, Backward_Speed)
+
                 Status = 2
             elif Status == 1 :
                 screen.addstr(0, 0, 'Enter : Stop    ')
-                pwm.set_pwm(3, 3, 200)
-                time.sleep(0.1)
-                pwm.set_pwm(3, 3, 400)
+
                 Status = 0
         elif char == -1:
             screen.addstr(0, 0, 'Enter :         ')
-            pwm.set_pwm(1, 1, 400)
+
 
 
         elif char == ord('w') :
             if Status == 1 and Forward_Speed < 650 :
                 Forward_Speed = Forward_Speed + 30
-                pwm.set_pwm(3, 3, Forward_Speed)
+
                 screen.addstr(0, 0, 'FASTER          ')
             elif Status == 2 and Backward_Speed > 190 :
                 Backward_Speed = Backward_Speed - 30
-                pwm.set_pwm(3, 3, Backward_Speed)
                 screen.addstr(0, 0, 'FASTER          ') 
         elif char == ord('s') :
             if Status == 1 and Forward_Speed > 440 :
                 Forward_Speed = Forward_Speed - 30                                  
-                pwm.set_pwm(3, 3, Forward_Speed)
+
                 screen.addstr(0, 0, 'SLOWER          ')
             elif Status == 2 and Backward_Speed < 350 :
                 Backward_Speed = Backward_Speed + 30
-                pwm.set_pwm(3, 3, Backward_Speed)
+
                 screen.addstr(0, 0, 'SLOWER          ')      
 
             
 except KeyboardInterrupt :
     print('EXIT....   ')
-    pwm.set_pwm(1, 1, 400)
-    time.sleep(0.5)
-    pwm.set_pwm(3, 3, 400)
-    time.sleep(0.5)
+
     curses.nocbreak(); screen.keypad(0); curses.echo()
     curses.endwin()
 
